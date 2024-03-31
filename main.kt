b15f014f8846cbdcdb6eaa4cc51c9cf1427f7ca4
Crystal <99391710+x200706@users.noreply.github.com>
2024-03-31 10:16:09 +0000

    Initial commit

---
+++ main.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+  // 變數宣告
+  var str = "一但我出現了我就是字串；我可以被重新賦值~" // 單引號只有char可用跟其他語言省去跳脫意思不同
+  val finalStr =  "等同於Java Final；另外變數一樣推薦小駝峰"
+
+  // 擴充函數 一種我不用繼承就能擴寫的概念 
+
+  // 空指針異常防護 
+
+  // Collection & functional 
+
+  // 同步異步
+
+  // 打開檔案
+
+  // 使用Java套件
+
+  // 解構聲明 類似Py迭代解包 for item in items
+  // 解包 Py也有 
+
+  // 函數內有函數
+
+  // 惰性 
+}
+
+// 類 
+// 建構子
+
+// Kotlin Shell
